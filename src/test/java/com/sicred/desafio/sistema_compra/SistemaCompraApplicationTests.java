280ec8326dc7f03b99d2bf385eeba366d7af5d33
SUELITON DE OLIVEIRA (X01142591190) <x01142591190@tcu.gov.br>
2024-10-14 13:20:03 -0300

    criando projeto

---
+++ src/test/java/com/sicred/desafio/sistema_compra/SistemaCompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicred.desafio.sistema_compra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
